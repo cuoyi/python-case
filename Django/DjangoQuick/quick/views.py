b70969652b46856c2e5351d0fe954867aea87c49
xiezhonghai <xiezhonghai@ybm100.com>
2019-10-30 15:52:29 +0800

    代码优化

--- Django/DjangoQuick/quick/views.py
+++ Django/DjangoQuick/quick/views.py
@@ -25,8 +25,8 @@ def index(request):
 
 # 小说查询页面
 def ebook(request):
-
     book_list = {}
+    book_name = ''
     if request.method == 'GET':
         book_name = request.GET.get('bookname')
         if not book_name is None:
@@ -42,24 +42,7 @@ def ebook(request):
 
     return render(request,
                   template_name='ebook.html',
-                  context={'books': book_list})
-
-
-# 小说查询页面
-def downBook(request):
-    if request.method == 'GET':
-        book_name = request.GET.get('bookname')
-        if not book_name is None:
-            print('输入的书籍名称为：%s' % book_name)
-            book_list = searchBook(book_name)
-        # book_list = {
-        #     1: {
-        #         'name': '诛仙之绝代剑仙',
-        #         'author': '浮世暮秋',
-        #         'url': 'http://www.biqukan.com/book/goto/id/34459'
-        #     }
-        # }
-
-    return render(request,
-                  template_name='ebook.html',
-                  context={'books': book_list})
+                  context={
+                      'books': book_list,
+                      'bookname': book_name
+                  })
